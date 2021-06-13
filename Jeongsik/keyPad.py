fbed03a343678a4959fa4f82153511c8b2594e4e
Zundal <ajs15010120@gmail.com>
2021-06-13 22:01:48 +0900

    fetch : upload algorithm

--- Jeongsik/keyPad.py
+++ Jeongsik/keyPad.py
@@ -39,7 +39,7 @@ def same_distance(num, hand, temp):
         else :
             return 'R'
     else :
-        
+
 def distribute_number(numbers, hand):
     result_array = []
     temp_num = {'left':0,'right':0}
@@ -62,7 +62,10 @@ def solution(numbers, hand):
     
     answer = ''
     return answer
+
 # 테스팅
-#solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], "right")
+solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], "right")
 #solution([7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2], "left")
 #solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], "right")
+
+#distribute_number()

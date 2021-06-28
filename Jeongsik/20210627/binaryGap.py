79b1042fe38ffceec8d8dda2f80a50d65421d29f
Zundal <ajs15010120@gmail.com>
2021-06-28 21:25:19 +0900

    업데이트

--- Jeongsik/20210627/binaryGap.py
+++ Jeongsik/20210627/binaryGap.py
@@ -16,12 +16,55 @@
     The function should return 0 if N doesn't contain a binary gap.
 
     For example, given N = 1041 the function should return 5, 
-    because N has binary representation 10000010001 and so its longest binary gap is of length 5. 
-    Given N = 32 the function should return 0, because N has binary representation '100000' and thus no binary gaps.
-    Write an efficient algorithm for the following assumptions:
-    N is an integer within the range [1..2,147,483,647].
+        because N has binary representation 10000010001 and so its longest binary gap is of length 5. 
+        Given N = 32 the function should return 0, 
+        because N has binary representation '100000' and thus no binary gaps.
+        Write an efficient algorithm for the following assumptions:
+        N is an integer within the range [1..2,147,483,647].
 '''
 
 def solution(n):
+    answer = ''
+    x = int(n / 2)
+    y = int(n % 2)
+    print(y)
+    for seq in range(x-1):
+        answer = answer + '0'
+    else :
+        answer = '1' + answer
     
+    print(answer)
     return 0
+
+solution(10)
+solution(101)
+#solution(1000)
+#solution(10000)
+
+'''
+1   1
+2   10
+3   11
+4   100
+5   101
+6   110
+7   111
+8   1000
+9   1001
+10  1010
+11  1011
+12  1100
+13  1101
+14  1110
+15  1111
+16  10000
+17  10001
+18  10010
+19  10011
+20  10100
+21  10101
+22  10110
+23  10111
+25  11000
+
+'''

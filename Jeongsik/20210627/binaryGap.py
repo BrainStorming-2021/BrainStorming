205acddb7e47eb31ef5f67bf303c9f0046daa5f4
Zundal <ajs15010120@gmail.com>
2021-07-04 11:22:56 +0900

    binary_modifiy

--- Jeongsik/20210627/binaryGap.py
+++ Jeongsik/20210627/binaryGap.py
@@ -1,22 +1,16 @@
+#-*- coding:utf-8 -*-
 def solution(n):
-    # 입력값 bin code 로 변환
-    binary = bin(n)
-    print(binary[2:])
-
-    # 들어오는 값 위치 확인
+    binary = bin(n)    
     binaryRange = []
     for index, value in enumerate(binary[2:]) :
-        # print(index, value, sep='|')
         if value == '1':
-            # binary gap range array
             binaryRange.append(index)
-
     return binaryGap(binaryRange)
-    
+
 def binaryGap(binaryRange):
     binaryGap = []
     for index in range(len(binaryRange)-1):
         binaryGap.append(binaryRange[index+1] - binaryRange[index] - 1)
     return max(binaryGap)
 
-print(solution(10000000))
+solution(10000000)

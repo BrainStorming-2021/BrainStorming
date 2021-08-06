4e4cd80cca650127f98545a48dc3b53c38cb1651
ajs <ajs15010120@gmail.com>
2021-08-06 09:12:10 +0900

    card algorithm solved

---
+++ Jeongsik/20210803/card.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+stack = []
+
+def solution (num):
+        
+        calculator= deque([seq for seq in range(1, num+1)])
+            recul(calculator)
+                
+                    return stack
+
+                # recursive
+                def recul(arr):
+                        if arr :
+                                    stack.append(arr.popleft())
+                                            if arr :
+                                                            arr.append(arr.popleft())
+                                                                    else :
+                                                                                    return stack
+                                                                                    return recul(arr)
+                                                                                    
+                                                                                print(solution(7))
+                                                                                complcache_start_auto_complete)OB

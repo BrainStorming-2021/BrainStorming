ff1a25d97b7097282759cc53b6c24dc5d78415fe
Zundal <ajs15010120@gmail.com>
2021-08-28 10:47:50 +0900

    문제 이동

--- Jeongsik/week18/intSort.py
+++ Jeongsik/week18/intSort.py
@@ -1,36 +1,35 @@
 '''
 문제
-N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+    N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
 
 입력
-첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000)이 주어진다. 
-둘째 줄부터 N개의 줄에는 수 주어진다. 
-이 수는 절댓값이 1,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
+    첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000)이 주어진다. 
+    둘째 줄부터 N개의 줄에는 수 주어진다. 
+    이 수는 절댓값이 1,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
 
 출력
-첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+    첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
 
-예제 입력 1  복사
+예제 입력 1  복사   예제 출력 1  복사
 5
-5
-2
-3
-4
-1
-예제 출력 1  복사
-1
-2
-3
-4
-5
+5                1   
+2                2   
+3                3   
+4                4   
+1                5
+
+
+
+
+
+
 '''
-import numpy as np
-
-a = np.random.rand(5)
-print(a)
-#import numpy as np
-#iNum = int(input())
- 
+import random as rd
+nStack = []
+iNum = int(input())
+while(iNum == len(nStack)):
 #for _ in range(iNum):
-#    rNum = np.random.randint(1, iNum)
-#    print(rNum)
+    ranNum = rd.randint(1, iNum)
+    if not ranNum in nStack :
+        nStack.append(ranNum)
+    print(nStack)

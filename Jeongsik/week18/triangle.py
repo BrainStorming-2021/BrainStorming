758b11ad8a4d848de9fb941b01f0620fa84b0201
Zundal <ajs15010120@gmail.com>
2021-08-28 12:42:25 +0900

    문제 푸는중

--- Jeongsik/week18/triangle.py
+++ Jeongsik/week18/triangle.py
@@ -32,7 +32,6 @@ N is an integer within the range [0..100,000];
 each element of array A is an integer within the range [−2,147,483,648..2,147,483,647].
 Copyright 2009–2021 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
 
-
 N개의 정수로 구성된 배열 A가 제공됩니다. 트리플릿(P, Q, R)은 0 ≤ P < R > N이면 삼각형이다.
 
 A[P] + A[Q] > A[R],
@@ -45,25 +44,53 @@ A[3] = 1 A[4] = 8 A[5] = 20
 삼중곱(0, 2, 4)은 삼각형입니다.
 
 함수 쓰기:
-
-클래스 솔루션 {public int 솔루션(int[] A); }
-
-즉, N개의 정수로 구성된 배열 A가 주어진 경우 이 배열에 대해 삼각형 삼중수열이 있으면 1을 반환하고 그렇지 않으면 0을 반환합니다.
-
-예를 들어, 다음과 같이 배열 A가 지정되어 있습니다.
+    클래스 솔루션 {public int 솔루션(int[] A); }
+    즉, N개의 정수로 구성된 배열 A가 주어진 경우 이 배열에 대해 삼각형 삼중수열이 있으면 1을 반환하고 그렇지 않으면 0을 반환합니다.
+    예를 들어, 다음과 같이 배열 A가 지정되어 있습니다.
 
 A[0] = 10A[1] = 2A[2] = 5
 A[3] = 1 A[4] = 8 A[5] = 20
-위에서 설명한 것처럼 함수는 1을 반환해야 합니다. 주어진 배열 A는 다음과 같습니다.
+    위에서 설명한 것처럼 함수는 1을 반환해야 합니다. 주어진 배열 A는 다음과 같습니다.
 
 A[0] = 10A[1] = 50A[2] = 5
 A[3] = 1
-함수가 0을 반환해야 합니다.
+    함수가 0을 반환해야 합니다.
 
 다음 가정에 대한 효율적인 알고리즘을 작성합니다.
 
 N은 [0] 범위 내의 정수입니다.100,000];
 배열 A의 각 요소는 [-2,147,483,648..2,147,483,647] 범위의 정수입니다.
 Codility Limited의 저작권 2009-2021. All Rights Reserved. 무단 복사, 출판 또는 공개가 금지됩니다.
-
 '''
+
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A):
+    # write your code in Python 3.6
+    idx = 0
+    stack = {}
+    
+    # 1. 패턴별 더하기
+    for P in A:
+        for Q in A:
+            if max(A) != Q : # 조건 A 의 max 값과 Q 가 같다면 마시막 시퀀스 이다.
+                R = Q + 1
+            if (P + Q) > R :
+                stack[idx]={P, Q, R}
+                # print('P Q R 값')
+                # print(P, Q, R)
+            idx = idx + 1
+    
+    # 2. 배열정리
+    for key in stack :
+        if len(stack[key]) == 2:
+            del stack[key]
+        #print(key, len(stack[key]), sep = '|')
+        
+    
+    pass
+
+
+solution([10, 2, 5, 1, 8, 20])
+# solution([10, 50, 5, 1])

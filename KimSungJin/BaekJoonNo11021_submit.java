9ad55cc8a5caa7cbb6cfee7b74049ed4fe835973
SUNGJIN_KIM <0ksj340@gmail.com>
2021-11-10 21:02:09 +0900

    11021 : 단계별로 풀어보기 : 백준

---
+++ KimSungJin/BaekJoonNo11021_submit.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BaekJoonNo11021_submit {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= count; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            bw.write("Case #" + i + ": " + (a + b) + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}

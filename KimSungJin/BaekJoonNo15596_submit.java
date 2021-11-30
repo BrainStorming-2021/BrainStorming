8b45498c875b0568229e27c1db305e310d84e76b
SUNGJIN_KIM <0ksj340@gmail.com>
2021-11-30 20:13:26 +0900

    15596 : 단계별로 풀어보기 : 백준

---
+++ KimSungJin/BaekJoonNo15596_submit.java
@@ -0,0 +1,9 @@
+public class BaekJoonNo15596_submit {
+    long sum(int[] a) {
+        long ans = 0;
+        for (long input : a) {
+            ans += input;
+        }
+        return ans;
+    }
+}
